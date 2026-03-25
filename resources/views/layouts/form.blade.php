0024c13ad6c28e0f18a37ae272104d91c203d877
DECKARDCAIM <cfallam2@miumg.edu.gt>
2026-03-25 09:58:28 -0600

    CORRECION MINIMA

--- resources/views/layouts/form.blade.php
+++ resources/views/layouts/form.blade.php
@@ -23,7 +23,6 @@
     <link rel="prefetch" href="{{ asset('vendor/tom-select/dist/js/tom-select.complete.min.js') }}">
     <link rel="prefetch" href="{{ asset('js/sweetalert2.all.min.js') }}">
     <link rel="prefetch" href="{{ asset('js/hs.theme-appearance.js') }}">
-    <link rel="prefetch" href="{{ route('home') }}">
     @yield('styles')
     <style data-hs-appearance-onload-styles>
         * {
